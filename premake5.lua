129ccaabfd5c2bd1c1be6d9941e51bb0a95df50a
Mohamd Johmani <mohamdaljohmani@gmail.com>
2025-05-09 19:51:29 +0300

    make backends individually disableable

--- premake5.lua
+++ premake5.lua
@@ -1,107 +1,138 @@
 project "nvrhi"
-	location (projectLocation)
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++latest"
-	staticruntime "off"
-
-	targetdir (binOutputDir)
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++latest"
+    staticruntime "off"
+    location (projectLocation)
+    targetdir (binOutputDir)
     objdir (IntermediatesOutputDir)
 
-	files
-	{
-		"include/**.h",
+    files {
 
-		"include/nvrhi/*.cppm",
-		"src/validation/**.h",
-		"src/validation/**.cpp",
-		"src/common/**.h",
-		"src/common/**.cpp",
-	
-		"src/vulkan/**.h",
-		"src/vulkan/**.cpp",
+        "include/**.h",
+        "src/validation/**.h",
+        "src/common/**.h",
 
-		"rtxmu/include/rtxmu/AccelStructManager.h",
-		"rtxmu/include/rtxmu/VulkanSuballocator.h",
-		"rtxmu/include/rtxmu/Suballocator.h",
-		"rtxmu/include/rtxmu/VkAccelStructManager.h",
-		"rtxmu/include/rtxmu/Logger.h",
+        "include/nvrhi/*.cppm",
+        "src/validation/**.cpp",
+        "src/common/**.cpp",
 
-		"rtxmu/src/VulkanSuballocator.cpp",
-		"rtxmu/src/Logger.cpp",
-		"rtxmu/src/VkAccelStructManager.cpp",
-
-		"*.lua",
-	}
-
-	includedirs
-    {
-		"include",
-		"rtxmu/include/",
-		"%{IncludeDir.Vulkan_Headers}",
+        "*.lua",
     }
 
-	defines
-	{
-		"NVRHI_WITH_RTXMU",
-		"NVRHI_SHARED_LIBRARY_BUILD"
-	}
+    removefiles {
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		defines
-		{
-			"NVRHI_HAS_VULKAN"
-		}
+        "src/common/dxgi-format.h",
+        "src/common/dxgi-format.cpp",
+    }
 
-	filter "system:windows"
-		systemversion "latest"
-		files
-		{
-			"src/common/dxgi-format.h",
-			"src/common/dxgi-format.cpp",
-			"src/d3d11/**.h",
-			"src/d3d11/**.cpp",
-	
-			"src/common/versioning.h",
-			"src/d3d12/**.h",
-			"src/d3d12/**.cpp",
-			
-			"rtxmu/include/rtxmu/D3D12AccelStructManager.h",
-			"rtxmu/include/rtxmu/D3D12Suballocator.h",
-			"rtxmu/src/D3D12Suballocator.cpp",
-			"rtxmu/src/D3D12AccelStructManager.cpp",
-	
-			_ACTION:match("vs") and "tools/nvrhi.natvis" or nil
-		}
+    if RHI.enableVulkan or RHI.enableD3D12 then
+        files {
 
-		links
-		{
-			"DXGI.lib",
-			"D3D11.lib",
-			"D3D12.lib",
-			"dxguid.lib"
-		}
+            "rtxmu/include/rtxmu/AccelStructManager.h",
+            "rtxmu/include/rtxmu/Suballocator.h",
+            "rtxmu/include/rtxmu/Logger.h",
+            "rtxmu/src/Logger.cpp",
+        }
+    end
 
-		defines 
-		{
-			"VK_USE_PLATFORM_WIN32_KHR",
-			"NOMINMAX",
-			"NVRHI_HAS_D3D11",
-			"NVRHI_HAS_D3D12",
-			"NVRHI_HAS_VULKAN",
-		}
-	
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "On"
+    includedirs {
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "On"
+        "include",
+        "rtxmu/include/",
+        "%{IncludeDir.Vulkan_Headers}",
+    }
 
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "Speed"
+    defines {
+
+        "NVRHI_WITH_RTXMU",
+        "NVRHI_SHARED_LIBRARY_BUILD"
+    }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        if RHI.enableVulkan then
+            defines { "NVRHI_HAS_VULKAN" , "VK_USE_PLATFORM_WIN32_KHR" }
+            files {
+                "src/vulkan/**.h",
+                "src/vulkan/**.cpp",
+
+                "rtxmu/include/rtxmu/VkAccelStructManager.h",
+                "rtxmu/include/rtxmu/VulkanSuballocator.h",
+                "rtxmu/src/VkAccelStructManager.cpp",
+                "rtxmu/src/VulkanSuballocator.cpp",
+            }
+        end
+
+    filter "system:windows"
+        systemversion "latest"
+        files {
+
+            "src/common/dxgi-format.h",
+            "src/common/dxgi-format.cpp",
+
+            _ACTION:match("vs") and "tools/nvrhi.natvis" or nil
+        }
+
+        links {
+
+            "DXGI.lib",
+            "dxguid.lib"
+        }
+
+        defines {
+
+            "NOMINMAX",
+        }
+
+        if RHI.enableD3D11 then
+            links { "D3D11.lib" }
+            defines { "NVRHI_HAS_D3D11" }
+            files {
+
+                "src/d3d11/**.h",
+                "src/d3d11/**.cpp",
+            }
+        end
+
+        if RHI.enableD3D12 then
+            links { "D3D12.lib" }
+            defines { "NVRHI_HAS_D3D12" }
+            files {
+
+                "src/d3d12/**.h",
+                "src/d3d12/**.cpp",
+
+                "rtxmu/include/rtxmu/D3D12AccelStructManager.h",
+                "rtxmu/include/rtxmu/D3D12Suballocator.h",
+                "rtxmu/src/D3D12AccelStructManager.cpp",
+                "rtxmu/src/D3D12Suballocator.cpp",
+            }
+        end
+
+        if RHI.enableVulkan then
+            defines { "NVRHI_HAS_VULKAN" , "VK_USE_PLATFORM_WIN32_KHR" }
+            files {
+
+                "src/vulkan/**.h",
+                "src/vulkan/**.cpp",
+                "rtxmu/include/rtxmu/VkAccelStructManager.h",
+                "rtxmu/include/rtxmu/VulkanSuballocator.h",
+                "rtxmu/src/VkAccelStructManager.cpp",
+                "rtxmu/src/VulkanSuballocator.cpp",
+            }
+        end
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "Speed"
         symbols "Off"
